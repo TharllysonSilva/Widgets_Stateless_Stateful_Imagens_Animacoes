b570b6f141b9070d84ae63545c604607eb889b17
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-06-07 13:22:22 -0300

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -11,89 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme:
-              ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 37, 204)),
-          useMaterial3: true,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tarefas'),
         ),
-        home: Container(
-            color: Colors.white,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(
-                      color: Colors.red,
-                      width: 100,
-                      height: 100,
-                    ),
-                    Container(
-                      color: Colors.blue,
-                      width: 50,
-                      height: 50,
-                    )
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(
-                      color: Colors.blue,
-                      width: 100,
-                      height: 100,
-                    ),
-                    Container(
-                      color: Colors.red,
-                      width: 50,
-                      height: 50,
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.cyan,
-                      height: 50,
-                      width: 50,
-                    ),
-                    Container(
-                      color: Colors.pinkAccent,
-                      height: 50,
-                      width: 50,
-                    ),
-                    Container(
-                      color: Colors.purple,
-                      height: 50,
-                      width: 50,
-                    )
-                  ],
-                ),
-                Container(
-                  color: Colors.amber,
-                  height: 30,
-                  width: 300,
-                  child: Text(
-                    'Diamante amarelo',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 28,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print('Você apertou o botão');
-                  },
-                  child: Text('Aperte o botão!'),
-                ),
-              ],
-            )));
+        body: Container(),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
   }
 }
