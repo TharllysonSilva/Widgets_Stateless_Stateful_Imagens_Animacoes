49ae18f5ecbb8296238d12b8cf70b17c34cb6b57
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-06-10 00:48:08 -0300

    assets removidos

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -24,27 +24,31 @@ class _InitialScreenState extends State<InitialScreen> {
           children: const [
             Task(
               nome: 'Aprender Flutter',
-              foto: 'assets/images/dash.jpg',
+              foto:
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
               dificuldade: 3,
             ),
             Task(
               nome: 'Andar de Bike',
-              foto: 'assets/images/bike.jpg',
+              foto:
+                  'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
               dificuldade: 2,
             ),
             Task(
               nome: 'Ler os livros',
-              foto: 'assets/images/livros.jpg',
+              foto:
+                  'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
               dificuldade: 4,
             ),
             Task(
               nome: 'Meditar',
-              foto: 'assets/images/meditar.jpeg',
+              foto:
+                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
               dificuldade: 5,
             ),
             Task(
               nome: 'Jogar video game',
-              foto: 'assets/images/jogar.jpg',
+              foto: 'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
               dificuldade: 1,
             ),
             SizedBox(
