645d92ea68ac33f31a68cda818e4352554cba87c
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-06-10 00:17:10 -0300

    Mudança na main de statefull para stateless

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,9 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
