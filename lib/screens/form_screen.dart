a51e436c028aa9de34ba0c3be49c0578b56fc658
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-06-12 14:17:16 -0300

    estrutura base do form

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -10,6 +10,19 @@ class FormScreen extends StatefulWidget {
 class _FormScreenState extends State<FormScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nova Tarefa'),
+      ),
+      body: Container(
+        height: 650,
+        width: 375,
+        decoration: BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(width: 3),
+        ),
+      ),
+    );
   }
 }
