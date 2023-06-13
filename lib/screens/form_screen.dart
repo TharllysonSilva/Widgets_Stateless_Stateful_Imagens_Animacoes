01faa219199b83946ede23761e4fee73dcab476e
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-06-13 16:54:42 -0300

    keyboardType

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -20,87 +20,90 @@ class _FormScreenState extends State<FormScreen> {
         title: const Text('Nova Tarefa'),
       ),
       body: Center(
-        child: Container(
-          height: 580,
-          width: 390,
-          decoration: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(width: 3),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  textAlign: TextAlign.center,
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Nome',
-                    fillColor: Colors.white70,
-                    filled: true,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  textAlign: TextAlign.center,
-                  controller: difficultyController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Dificuldade',
-                    fillColor: Colors.white70,
-                    filled: true,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  onChanged: (text) {
-                    setState(() {});
-                  },
-                  textAlign: TextAlign.center,
-                  controller: imageController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Imagem',
-                    fillColor: Colors.white70,
-                    filled: true,
-                  ),
-                ),
-              ),
-              Container(
-                  height: 100,
-                  width: 70,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(width: 2, color: Colors.blue)),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      imageController.text,
-                      errorBuilder: (BuildContext context, Object exception,
-                          StackTrace? stackTrace) {
-                        return Image.network(
-                            'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large');
-                      },
-                      fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Container(
+            height: 580,
+            width: 390,
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(width: 3),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Nome',
+                      fillColor: Colors.white70,
+                      filled: true,
                     ),
-                  )),
-              ElevatedButton(
-                  onPressed: () {
-                    print(nameController.text);
-                    print(int.parse(difficultyController.text));
-                    print(imageController.text);
-                  },
-                  child: const Text('Adicionar!'))
-            ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center,
+                    controller: difficultyController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Dificuldade',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    onChanged: (text) {
+                      setState(() {});
+                    },
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.url,
+                    controller: imageController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Imagem',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 100,
+                    width: 80,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(width: 2, color: Colors.blue)),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        imageController.text,
+                        errorBuilder: (BuildContext context, Object exception,
+                            StackTrace? stackTrace) {
+                          return Container();
+                        },
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+                ElevatedButton(
+                    onPressed: () {
+                      print(nameController.text);
+                      print(int.parse(difficultyController.text));
+                      print(imageController.text);
+                    },
+                    child: const Text('Adicionar!'))
+              ],
+            ),
           ),
         ),
       ),
