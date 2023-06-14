0d40d4adfee1dccabfe38eebf6b502a7d6cb47bc
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-06-14 19:00:02 -0300

    navigator.pop

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -133,7 +133,7 @@ class _FormScreenState extends State<FormScreen> {
                           ScaffoldMessenger.of(context).showSnackBar(
                             const SnackBar(
                               content: Text('Nova tarefa salva!'),
-                            ),
+                            ),,
                           );
                         }
                       },
