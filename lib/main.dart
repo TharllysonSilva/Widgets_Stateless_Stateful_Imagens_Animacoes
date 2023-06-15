8b437aeba0ed07b016a0339bcb51747afdd971c4
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-06-15 17:36:50 -0300

    taskInherited

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_project/data/task_inherited.dart';
 import 'package:flutter_test_project/screens/initial_screen.dart';
 //import 'package:flutter_test_project/screens/initial_screen.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home: TaskInherited(child: const InitialScreen()),
     );
   }
 }
