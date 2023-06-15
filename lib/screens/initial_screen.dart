0a23758a0707cbe58c80146c1ce90a8ef0209e14
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-06-15 17:37:23 -0300

    atritbuindo responsabilidade para o inherited

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -20,35 +20,6 @@ class _InitialScreenState extends State<InitialScreen> {
       ),
       body: ListView(
         children: const [
-          Task(
-            nome: 'Aprender Flutter',
-            foto:
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-            dificuldade: 3,
-          ),
-          Task(
-            nome: 'Andar de Bike',
-            foto:
-                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
-            dificuldade: 2,
-          ),
-          Task(
-            nome: 'Ler os livros',
-            foto:
-                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-            dificuldade: 4,
-          ),
-          Task(
-            nome: 'Meditar',
-            foto:
-                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-            dificuldade: 5,
-          ),
-          Task(
-            nome: 'Jogar video game',
-            foto: 'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
-            dificuldade: 1,
-          ),
           SizedBox(
             height: 80,
           )
