50393b0efc62fc55aad797f098f861dabde1dd18
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-06-16 12:53:40 -0300

    removendo const desnecessarios

--- lib/data/task_inherited.dart
+++ lib/data/task_inherited.dart
@@ -8,27 +8,27 @@ class TaskInherited extends InheritedWidget {
   }) : super(key: key, child: child);
 
   final List<Task> taskList = [
-    const Task(
+    Task(
         nome: 'Aprender Flutter',
         foto:
             'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
         dificuldade: 3),
-    const Task(
+    Task(
         nome: 'Andar de Bike',
         foto:
             'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
         dificuldade: 2),
-    const Task(
+    Task(
         nome: 'Ler os livros',
         foto:
             'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
         dificuldade: 4),
-    const Task(
+    Task(
         nome: 'Meditar',
         foto:
             'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
         dificuldade: 5),
-    const Task(
+    Task(
         nome: 'Jogar video game',
         foto: 'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
         dificuldade: 1),
